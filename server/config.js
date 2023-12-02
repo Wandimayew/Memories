5506d7a771123ed2c03f0a82e883b73098d09e2b
Wandimayew <wandimayew8@gmail.com>
2023-12-02 10:44:28 +0300

    correcting url request

--- server/config.js
+++ server/config.js
@@ -1,1 +1,1 @@
-export default URL ="mongodb+srv://wandimayew8:AzIeAVjVLaKvN5Gi@cluster0.osrb001.mongodb.net/?retryWrites=true&w=majority";
+export default URL ="mongodb+srv://wandimayew8:AzIeAVjVLaKvN5Gi@cluster0.osrb001.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=60000";
